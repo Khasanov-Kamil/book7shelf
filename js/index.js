ba33f5699df544a2b0f62518fb42e5663aa7068e
Khasanov-Kamil <Daba9889@gmail.com>
2022-06-19 11:50:36 +0300

    Upload Filles

---
+++ js/index.js
@@ -0,0 +1,35 @@
+console.log('hi')
+let brg = document.querySelector('.burger'),
+    online = document.querySelector('.phone__info span');
+brg.addEventListener('click', () =>{
+  brg.classList.toggle('burger__active')
+  document.querySelector('.nav__links').classList.toggle('nav__links-active')
+})
+fetch('https://api.mcsrvstat.us/2/book7shelf.ru').then((response) => { 
+  return response.json();
+}).then((data) =>{ 
+  if (data.online) {
+    online.innerHTML = data.players.online + " из " + data.players.max
+  }else{
+    
+  }
+});
+
+
+
+
+
+const swiper = new Swiper('.swiper', { 
+  // Optional parameters 
+  direction: 'horizontal',
+  effect: "cards",
+  grabCursor: true,
+  loop: true, 
+  // If we need pagination 
+  pagination: { el: '.swiper-pagination', }, 
+  // Navigation arrows 
+  navigation: { nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev', }
+  // And if we need scrollbar 
+  
+});
